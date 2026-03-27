0ffb5c36e273eac9032c44fdb0b269a4a92ebb92
Nicky Wakim <wakim@ohsu.edu>
2026-03-27 15:30:58 -0700

    adding more to the site

--- class_dates.R
+++ class_dates.R
@@ -27,43 +27,43 @@ hw1 = cal_dates[21]
 ## Week 4 dates 
 w4d1 = cal_dates[22]
 w4d2 = cal_dates[24]
-hw2 = cal_dates[28]
+hw2 = cal_dates[35]
 q1_open = cal_dates[24]
 q1_close = cal_dates[28]
 
 ## Week 5 dates
 w5d1 = cal_dates[29]
 w5d2 = cal_dates[31]
-lab2 = cal_dates[35]
+lab2 = cal_dates[28]
 
 ## Week 6 dates
 w6d1 = cal_dates[36]
 w6d2 = cal_dates[38]
-hw3 = cal_dates[42]
+hw3 = cal_dates[49]
 
 ## Week 7 dates
 w7d1 = cal_dates[43]
 w7d2 = cal_dates[45]
-lab3 = cal_dates[49]
+lab3 = cal_dates[42]
 q2_open = cal_dates[45]
 q2_close = cal_dates[49]
 
 ## Week 8 dates
 w8d1 = cal_dates[50]
 w8d2 = cal_dates[52]
-hw4 = cal_dates[56]
+hw4 = cal_dates[63]
 
 ## Week 9 dates
 w9d1 = cal_dates[57]
 w9d2 = cal_dates[59]
-lab4 = cal_dates[63]
+lab4 = cal_dates[56]
 
 ## Week 10 dates
 w10d1 = cal_dates[64]
 w10d2 = cal_dates[66]
 hw5 = cal_dates[70]
-q3_open = cal_dates[66]
-q3_close = cal_dates[70]
+q3_open = cal_dates[59]
+q3_close = cal_dates[63]
 
 ## Week 11 dates
 w11d1 = cal_dates[71]
